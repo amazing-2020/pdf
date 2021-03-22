374a9a0a4d921b0f89ee4086d9fa9f64f70cf6a0
turing <lyf15999687770@gmail.com>
2021-03-22 12:21:27 +0800

    03.22

--- C/code case 70.c
+++ C/code case 70.c
@@ -4,7 +4,7 @@ int main()
 {
     char str[20];
     printf("Enter a value \n");
-    fgets(str, sizeof(str), stdin);    
+    fgets(str, sizeof(str), stdin);
     printf("\n %d You entered\n", sizeof(str));
     puts(str);
     

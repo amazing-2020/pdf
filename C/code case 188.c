1a863aac3797dce789a056f170451357963f3b0e
turing <lyf15999687770@gmail.com>
2021-05-17 11:56:44 +0800

    05.17

--- C/code case 188.c
+++ C/code case 188.c
@@ -3,12 +3,12 @@
 int main(void) {
     char c;
     printf("Please enter a letter. \n");
-    scanf("%c", c);
+    scanf("%c", &c);
     if ((c>='A' && c<='Z') || (c>='a' && c<='z')) {
         if (c=='A'||c=='E'||c=='I'||c =='O'||c=='U'||c=='a'||c=='e'||c=='i'||c =='o'||c=='u') {
-            printf("%c is a vowell",c);
+            printf("%c is a vowell \n",c);
         } else {
-            printf("%c is a consonant",c);
+            printf("%c is a consonant \n",c);
         }
     } else {
         printf("Error input. \n");

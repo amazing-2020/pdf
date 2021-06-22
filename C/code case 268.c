bceb68a7e4e479211c282333dad5914634b88c0a
turing <lyf15999687770@gmail.com>
2021-06-22 15:14:13 +0800

    06.22

--- C/code case 268.c
+++ C/code case 268.c
@@ -14,7 +14,7 @@ int main(void) {
             if (num.i%2==0 && num.j%2==0) {
                 num.n = (num.i+num.j)/2;
                 num.x = num.n*num.n - 268;
-                int n2 = (num.j-num.i)/2 ;
+                int n2 = (num.j-num.i)/2;
                 printf("i = %d, x = %d , n = %d, %d\n", num.i, num.x, num.n, n2);
                 printf("%lu \n", sizeof(num));
             }

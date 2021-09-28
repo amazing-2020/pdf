0e18d4a89e486abbb8291350b30a1dfa483de405
turing <amazing-2020>
2021-09-28 13:09:19 +0800

    First python 09.28

---
+++ Python/code case/code case 1.py
@@ -0,0 +1,18 @@
+if True:
+    print("True")
+else:
+    print("False")
+str2 = '123456789'
+print(str2)
+print(str2[0:-1])
+print(str2[0])
+print(str2[2:5])
+print(str2[2:])
+print(str2[1:5:2])
+print(str2 * 2)
+print(str2 + 'Hello')
+
+print('---------------------------')
+
+print('hello\nbaidu')
+print(r'hello\ngoogle')

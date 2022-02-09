e4ecbab602d8574566824024bda0c4ce0658e98c
turing <amazing-2020>
2022-02-09 16:55:46 +0800

    02.09

--- C/cPrimerPlus/Exercise12_1.c
+++ C/cPrimerPlus/Exercise12_1.c
@@ -1,21 +1,20 @@
 #include "stdio.h"
-int units = 0;
-void critics(void);
+void critics(int * num);
 int main(void)
 {
-  extern int units;
+  int units = 0;
 
   printf("How many pounds to a firkin of butter? \n");
   scanf("%d", &units);
   while (units != 56)
-    critics();
+    critics(&units);
   printf("You must have looked it up! \n");
 
   return 0;
 }
 
-void critics(void)
+void critics(int * num)
 {
   printf("No luck, my friend. Try again. \n");
-  scanf("%d", &units);
+  scanf("%d", num);
 }

345ba53ca79cf33d6e43a60af09c1618c5ce249e
turing <amazing-2020>
2022-04-04 12:56:51 +0800

    04.04

--- C/cPrimerPlus/Exercise13_13.c
+++ C/cPrimerPlus/Exercise13_13.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define Numfile "Number13_12.txt"
-#define DESTINATION "Result13_12.txt"
+#define DESTINATION "Result13_13.txt"
+#define ROW 20
+#define LIST 31
 int main(void)
 {
   FILE *fp, *destination;
-  char array[20][31] = {0}, ch = 0;
-  char temp[31];
+  char array[ROW][LIST] = {0}, ch = 0;
+  char temp[LIST];
   int i = 0, j = 0;
 
   if ((fp = fopen(Numfile, "r")) == NULL)
@@ -14,7 +16,7 @@ int main(void)
     fprintf(stderr, "Can't open the file %s", Numfile);
     exit(EXIT_FAILURE);
   }
-  while ((ch = fgetc(fp)) != EOF && i < 20)
+  while ((ch = fgetc(fp)) != EOF && i < ROW)
   {
     if (ch != ' ')
     {
@@ -45,23 +47,23 @@ int main(void)
       }
       j++;
     }
-    if (j == 31) // 30 characters and a '\n'
+    if (j == LIST) // 30 characters and a '\n'
     {
       j = 0;
       i++;
     }
   }
   fclose(fp);
-  array[19][30] = '\0';
+  array[ROW-1][LIST-1] = '\0';
 
   if ((destination = fopen(DESTINATION, "w")) == NULL)
   {
     fprintf(stderr, "Can't open the file %s", DESTINATION);
     exit(EXIT_FAILURE);
   }
-  for (int k = 0; k < 20; ++k)
+  for (int k = 0; k < ROW; ++k)
   {
-    if (k == 19)
+    if (k == ROW-1)
       sprintf(temp, "%s", array[k]);
     else
       sprintf(temp, "%30s\n", array[k]);//30 + 1 = 31

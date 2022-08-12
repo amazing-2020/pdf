24abea7e679a40aafd04303a2e66359227a2dd01
fix-bugs <lyf15999687770@gmail.com>
2022-08-13 01:01:32 +0800

    08.12

---
+++ Python/PythonCrashCourse/chapter7/even_or_odd.py
@@ -0,0 +1,7 @@
+number = input("Enter a number, and I'll tell you if it's a even or a odd: ")
+number = int(number)
+
+if number % 2 == 0:
+    print(f"\nYhe number {number} is a even.")
+else:
+    print(f"\nYhe number {number} is a odd.")
